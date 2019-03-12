f880a92f08917921206e1cbae0d9494744bd2450
who8mahrice <kvu4@uh.edu>
2019-03-12 09:43:00 -0500

    compares list of required items and current list

--- STUFF/compare_items.pl
+++ STUFF/compare_items.pl
@@ -1,6 +1,39 @@
 
 
 # PASSING BY REFERENCE (Preferred Way): Compares an array and a hash to see if there are any missing 
+# required items (array) that is needed in the hash. AND adds a list of required items to items list
+sub check_required_items {
+my $who = shift;
+my $items = shift;
+my %whose_items = map { $_, 1 } @$items;
+
+my @required = qw(preserver sunscreen water_bottle jacket);
+my @missing = ( );
+
+for my $item (@required) {
+	unless ( $whose_items{$item} ) { # not found in list?
+		print "$who is missing $item.\n";
+		push @missing, $item;
+ }
+}
+
+if (@missing) {
+	print "Adding @missing to @$items for $who.\n";
+		push @$items, @missing;
+ }
+}
+my @gilligan = qw(red_shirt hat lucky_socks water_bottle);
+check_required_items('gilligan', \@gilligan);
+
+my @skipper = qw(blue_shirt hat jacket preserver sunscreen);
+check_required_items('skipper', \@skipper);
+
+my @professor = qw(sunscreen water_bottle slide_rule batteries radio);
+check_required_items('professor', \@professor);
+
+#----------------------------------------------------------------------------------------------------
+
+# PASSING BY REFERENCE (Preferred Way): Compares an array and a hash to see if there are any missing 
 # required items (array) that is needed in the hash.
 sub check_required_items {
 my $who = shift;
